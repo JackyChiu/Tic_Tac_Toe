9d418822a7c328b55e2d1bc2558a9c7d00dc4149
Jacky Chiu <jacky.chiu888@gmail.com>
2015-05-23 22:47:22 -0400

    close funtion header

---
+++ Tic_Tac_Toe/include/close.h
@@ -0,0 +1,21 @@
+//
+//  close.h
+//  Tic_Tac_Toe
+//
+//  Created by Jacky Chiu on 2015-05-23.
+//  Copyright (c) 2015 Jacky Chiu. All rights reserved.
+//
+
+#ifndef Tic_Tac_Toe_close_h
+#define Tic_Tac_Toe_close_h
+
+#include <iostream>
+#include <SDL2/SDL.h>
+#include <SDL2_image/SDL_image.h>
+#include <string>
+
+using namespace std;
+
+void close();
+
+#endif
---
+++ Tic_Tac_Toe/src/close.cpp
@@ -0,0 +1,9 @@
+//
+//  close.cpp
+//  Tic_Tac_Toe
+//
+//  Created by Jacky Chiu on 2015-05-23.
+//  Copyright (c) 2015 Jacky Chiu. All rights reserved.
+//
+
+#include <stdio.h>
