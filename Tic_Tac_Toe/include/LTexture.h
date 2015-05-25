dc58fef8f4175e7c3f88c6c57e7fc5318b9d2133
Jacky Chiu <jacky.chiu888@gmail.com>
2015-05-24 23:14:58 -0400

    Added texture class

---
+++ Tic_Tac_Toe/include/LTexture.h
@@ -0,0 +1,33 @@
+//
+//  LTexture.h
+//  Tic_Tac_Toe
+//
+//  Created by Jacky Chiu on 2015-05-24.
+//  Copyright (c) 2015 Jacky Chiu. All rights reserved.
+//
+
+#ifndef Tic_Tac_Toe_LTexture_h
+#define Tic_Tac_Toe_LTexture_h
+
+#include <iostream>
+#include <SDL2/SDL.h>
+#include <SDL2_image/SDL_image.h>
+#include <string>
+
+class LTexture
+{
+public:
+    LTexture();
+    ~LTexture();
+    void loadFromFile(string);
+    void free();
+    void render(int x,int y,SDL_Rect *clip=NULL);
+    int getheight();
+    int getwidth();
+private:
+    SDL_Texture *mTexture;
+    int width;
+    int height;
+};
+
+#endif
---
+++ Tic_Tac_Toe/src/LTexture.cpp
@@ -0,0 +1,9 @@
+//
+//  LTexture.cpp
+//  Tic_Tac_Toe
+//
+//  Created by Jacky Chiu on 2015-05-24.
+//  Copyright (c) 2015 Jacky Chiu. All rights reserved.
+//
+
+#include <stdio.h>
