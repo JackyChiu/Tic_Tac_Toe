b128e36c0060055ffa3cda7a01578aca6c9c6506
Jacky Chiu <jacky.chiu888@gmail.com>
2015-05-25 19:17:33 -0400

    Added LButton class

--- Tic_Tac_Toe/include/LButton.h
+++ Tic_Tac_Toe/include/LButton.h
@@ -9,5 +9,35 @@
 #ifndef Tic_Tac_Toe_LButton_h
 #define Tic_Tac_Toe_LButton_h
 
+#include <iostream>
+#include <SDL2/SDL.h>
+#include <SDL2_image/SDL_image.h>
+#include <string>
+
+using namespace std;
+
+const int Total_Buttons=9;
+
+enum LButtonSprite
+{
+    BUTTON_SPRITE_X,
+    BUTTON_SPRITE_O,
+    BUTTON_SPRITE_TOTAL
+};
+
+class LButton
+{
+public:
+    LButton();
+    void setposition(int x,int y);
+    void handleEvent(SDL_Event* e);
+    void render();
+private:
+    SDL_Point mPosition;
+    LButtonSprite CurrentSprite;
+};
+
+extern SDL_Rect SpriteClips[BUTTON_SPRITE_TOTAL];
+extern LButton Buttons[Total_Buttons];
 
 #endif
--- Tic_Tac_Toe/src/LButton.cpp
+++ Tic_Tac_Toe/src/LButton.cpp
@@ -5,5 +5,33 @@
 //  Created by Jacky Chiu on 2015-05-25.
 //  Copyright (c) 2015 Jacky Chiu. All rights reserved.
 //
+#include "LButton.h"
+#include "LTexture.h"
 
-#include <stdio.h>
+SDL_Rect SpriteClips[BUTTON_SPRITE_TOTAL];
+LButton Buttons[Total_Buttons];
+
+LButton::LButton()
+{
+    mPosition.x=0;
+    mPosition.y=0;
+}
+
+void LButton::setposition(int x,int y)
+{
+    mPosition.x=x;
+    mPosition.y=y;
+}
+
+void LButton::handleEvent(SDL_Event* e)
+{
+    if(e->type==SDL_MOUSEBUTTONDOWN)
+    {
+        CurrentSprite=BUTTON_SPRITE_X;
+    }
+}
+
+void LButton::render()
+{
+    ButtonSpriteSheet.render(mPosition.x,mPosition.y,&SpriteClips[CurrentSprite]);
+}
