9ab45018f486ece0f38aaa77ce16020c7c68ae21
Jacky Chiu <jacky.chiu888@gmail.com>
2015-05-26 19:40:11 -0400

    Added function to determine which button was pressed

---
+++ Tic_Tac_Toe/include/whichButton.h
@@ -0,0 +1,21 @@
+//
+//  whichButton.h
+//  Tic_Tac_Toe
+//
+//  Created by Jacky Chiu on 2015-05-26.
+//  Copyright (c) 2015 Jacky Chiu. All rights reserved.
+//
+
+#ifndef Tic_Tac_Toe_whichButton_h
+#define Tic_Tac_Toe_whichButton_h
+
+#include <iostream>
+#include <SDL2/SDL.h>
+#include <SDL2_image/SDL_image.h>
+#include <string>
+
+using namespace std;
+
+int whichButton(int,int);
+
+#endif
---
+++ Tic_Tac_Toe/src/whichButton.cpp
@@ -0,0 +1,22 @@
+//
+//  whichButton.cpp
+//  Tic_Tac_Toe
+//
+//  Created by Jacky Chiu on 2015-05-26.
+//  Copyright (c) 2015 Jacky Chiu. All rights reserved.
+//
+
+#include "whichButton.h"
+#include "LButton.h"
+
+/* board layout:
+    0, 1, 2,
+    3, 4, 5,
+    6, 7, 8
+ */
+
+int whichButton(int x,int y)
+{
+    //if(x<Buttons[0].getx()&&x>(Buttons[0].getx()//find alternative to width))
+    return 0;
+}
